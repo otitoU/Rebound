90ec6f5dc55aeb2d9c828ddda8c1935b66c8b71b
Otito Chukwuemeka Udedibor <uwollohpatrick12@gmail.com>
2023-09-05 09:10:04 -0500

    Initial Commit

---
+++ Rebound/ReboundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReboundApp.swift
+//  Rebound
+//
+//  Created by Otito Udedibor on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ReboundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
